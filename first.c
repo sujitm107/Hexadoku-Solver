4b7faecc08b5485700addd9531d4f7d464252307
Sujit Molleti <sujit.molleti@rutgers.edu>
2019-10-17 15:47:23 -0400

    finished but have to free

--- first.c
+++ first.c
@@ -9,6 +9,8 @@ int* checkCol(char**, int, int*);
 char** solveSudoku(char**);
 int getValueOfChar(char x);
 char getCharOfValue(int x);
+int validSudoku(char**);
+void printArray(int*);
 
 char** allocate_array(int rows, int cols){
 
@@ -16,86 +18,148 @@ char** allocate_array(int rows, int cols){
 	for(int i = 0; i<rows; i++){
 		arr[i] = malloc(cols * sizeof(char));
 	}
-
 	return arr;
+}
+
+int validSudoku(char** board){
+		
+	for(int i = 0; i<16; i++){
+		int* validRow = malloc(16*sizeof(int)); //automatically sets all values to 0
+		int* validCol = malloc(16*sizeof(int));
+		
+		validRow = checkRow(board, i, validRow);
+		//printf("Index: %d\n", i);
+		//printArray(validRow);
+		validCol = checkCol(board, i, validCol);
+		//printArray(validCol);
+		
+		for(int j = 0; j<16; j++){
+			if(validRow[j] > 1 || validCol[j] > 1){
+				free(validRow);
+				free(validCol);
+				return -1;
+			}
+		}
+		
+		free(validRow);
+		free(validCol);
+	}
+	
+	for(int r = 0; r<16; r+=4){
+		for(int c = 0; c<16; c+=4){
+			int* validSub = malloc(16*sizeof(int));
+			//printf("Index: %d\n", r+(c/4));
+			validSub = checkSubGrid(board, r, c, validSub);
+			//printArray(validSub);
+			
+			for(int j = 0; j<16; j++){
+				if(validSub[j] > 1){
+					free(validSub);
+					return -1;
+				}
+			}
+			
+			free(validSub);
+		}
+	}
+	
+	
+	return 0;
 }
 
 char** solveSudoku(char** board){
 	char** temp = board;
 	
-	for(int r = 0; r<16; r++){
-		for(int c = 0; c<16; c++){
-			if(temp[r][c] == '-'){
+	int x = validSudoku(board);
+	int run = 0;
+	int runLimit = 1;
+	
+	if(x == -1){
+		//printf("Not valid sudoku\n");
+		return NULL;
+	}
+	
+	do{
+		run++;
+		for(int r = 0; r<16; r++){
+			for(int c = 0; c<16; c++){
 				
-				int* present = malloc(16*sizeof(int));
-				
-				for(int i = 0; i<16; i++){
-					present[i] = -1;
-				}
-				
-				present = checkRow(board, r, present);
-				present = checkCol(board, c, present);
-				present = checkSubGrid(board, r, c, present);
-//	Prints present array			
-				for(int i = 0; i<16; i++){
-					printf("%d ", present[i]);
-				}
-				
-				printf("\n");
-
-
-				int index = 0;
-				while(present[index] != -1){
-					index++;
-					if(index == 16){ //this means that there are no options to put into the board, present is all 0s
-						return NULL;
+				if(temp[r][c] == '-'){	
+					int* present = malloc(16*sizeof(int));
+					
+					for(int i = 0; i<16; i++){
+						present[i] = 0;
+					}			
+					present = checkRow(board, r, present);
+					present = checkCol(board, c, present);
+					present = checkSubGrid(board, r, c, present);
+		/*	Prints present array			
+					for(int i = 0; i<16; i++){
+						printf("%d  ", present[i]);
 					}
-				}
-				
-				free(present);
-				
-				char fillIn = getCharOfValue(index);
-				
-				//printf("r: %d, c: %d\n", r, c);
-				
-				
-				temp[r][c] = fillIn;
-				
-			/*
-				find which numbers are not present,
-					declare present array and fill with 0s int* present = malloc(16*sizeof(int));
-					 FOR CHECKS [10 = a, 11 = b, 12 = c, 13 = d, 14 = e, 15 = f]
-					check row ------ present = checkRow(char** board, int* present, int row);
-					check col ------ present = checkCol(char** board, int* present, int col);
-					check subgrid ------ present = checkSubGrid(char** board, int row, int col);
-				int fillNumber = findNumber(present); (returns -1 if there if there is a dependency, or if no possible values)
-				if(fillNumber > 0){
-					temp[r][c] = some value, must convert to character
-				}
-					temp[r][c] = 'X';
-				
-			*/
+					
+					printf("\n");
+		*/
+					
+					//checking for multiple options
+					int options = 0;
+					for(int i = 0; i<16; i++){
+						if(present[i] == 0){
+							options++;
+						}
+					}
+					
+					if(options > 1){
+						runLimit++;
+						break;
+					}
+					
 
-			
+					int index = 0;
+					while(present[index] != 0){
+						index++;
+						if(index == 16){ //this means that there are no options to put into the board, present is all 0s
+							printf("r: %d, c: %d\n", r, c);
+							printf("No options\n");
+							return NULL;
+						}
+					}
+					
+					char fillIn = getCharOfValue(index);
+					temp[r][c] = fillIn;
+					free(present);
+				}
+				/*
+					find which numbers are not present,
+						declare present array and fill with 0s int* present = malloc(16*sizeof(int));
+						 FOR CHECKS [10 = a, 11 = b, 12 = c, 13 = d, 14 = e, 15 = f]
+						check row ------ present = checkRow(char** board, int* present, int row);
+						check col ------ present = checkCol(char** board, int* present, int col);
+						check subgrid ------ present = checkSubGrid(char** board, int row, int col);
+					int fillNumber = findNumber(present); (returns -1 if there if there is a dependency, or if no possible values)
+					if(fillNumber > 0){
+						temp[r][c] = some value, must convert to character
+					}
+						temp[r][c] = 'X';
+					
+				*/
+
 			}
 		}
-	}
+	}while(run < runLimit);
 		
 	return temp;
 }
 
 int main(int argc, char** argv){
-	
 	if(argc != 2){
 		printf("insufficient arguments\n");
 		exit(0);
 	}
-
 	FILE* fp = fopen(argv[1], "r");
 	if(fp == NULL){
 		printf("error\n");
 	}
-	
 	char num;
 	char** board = allocate_array(16, 16);
 	
@@ -106,7 +170,6 @@ int main(int argc, char** argv){
 			}
 		}	
 	}
-	
 	board = solveSudoku(board);
 	printBoard(board);
 	return 0;
@@ -119,7 +182,7 @@ int* checkRow(char** board, int row, int* present){
 	for(int c = 0; c<16; c++){
 		if(board[row][c] != '-'){
 			int value = getValueOfChar(board[row][c]);
-			temp[value] = 0;
+			temp[value]++;
 		}		
 	}
 	return temp;
@@ -132,7 +195,7 @@ int* checkCol(char** board, int col, int* present){
 	for(int r = 0; r<16; r++){
 		if(board[r][col] != '-'){
 			int value = getValueOfChar(board[r][col]);
-			temp[value] = 0;
+			temp[value]++;
 		}		
 	}
 	return temp;
@@ -177,7 +240,6 @@ int getValueOfChar(char x){
 int* checkSubGrid(char** board, int row, int col, int* present){
 
 	int* temp = present;
-
 	int x = row%4;
 	int y = col%4;
 
@@ -186,12 +248,21 @@ int* checkSubGrid(char** board, int row, int col, int* present){
 			//printf("%c\t", board[i][j]);    --------- FOR PRINTING
 			if(board[i][j] != '-'){
 				int value = getValueOfChar(board[i][j]);
-				temp[value] = 0;
+				temp[value]++;
 			}
 		}
 		//printf("\n");  ----- FOR PRINTING
 	}
 	return temp;
+}
+
+void printArray(int* arr){
+
+	for(int i = 0; i<16; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	printf("\n");
 }
 
 void printBoard(char** arr){
